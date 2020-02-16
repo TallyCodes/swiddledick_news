8d3480d1b32211a4a6b73b68b8668e6fdf749952
Jesse Anderson <jesse.c.anderson@icloud.com>
2020-02-15 22:14:52 -0500

    updated with webview

--- lib/views/detail_page.dart
+++ lib/views/detail_page.dart
@@ -1,62 +1,65 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:swiddledick_news/models/Item.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class DetailPage extends StatelessWidget {
+class DetailPage extends StatefulWidget {
   DetailPage({@required this.selectedItem});
-
+  
   final Item selectedItem;
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  Completer<WebViewController> _controller = Completer<WebViewController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: <Widget>[
-          Card(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Container(
-                  child: Text(
-                    '${selectedItem.title}',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Container(
-                  child: Column(
-                    children: <Widget>[
-                      Text(
-                        '${selectedItem.url}',
-                      ),
-                      Text(
-                        'This is where the story will go.',
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Card(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Text(
-                  'This is where the comments will go.',
-                )
-              ],
-            ),
-          ),
-        ],
+      body: MainWebView(
+        selectedItem: widget.selectedItem,
+        controller: _controller,
       ),
+    );
+  }
+}
+
+// class DetailPage extends StatelessWidget {
+//   DetailPage({@required this.selectedItem});
+
+//   final Item selectedItem;
+//   Completer<WebViewController> _controller = Completer<WebViewController>();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: MainWebView(selectedItem: selectedItem, controller: _controller),
+//     );
+//   }
+// }
+
+class MainWebView extends StatelessWidget {
+  const MainWebView({
+    Key key,
+    @required this.selectedItem,
+    @required Completer<WebViewController> controller,
+  })  : _controller = controller,
+        super(key: key);
+
+  final Item selectedItem;
+  final Completer<WebViewController> _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: selectedItem.url,
+      javascriptMode: JavascriptMode.unrestricted,
+      onWebViewCreated: (WebViewController webViewController) {
+        _controller.complete(webViewController);
+      },
     );
   }
 }
