c8888bd64bfba00280f2984fadb1d99ea586328d
Martin <mchurch89@gmail.com>
2023-07-07 13:03:42 -0400

    Refresh indicator and other updates (#9)
    * Refresh indicator and other updates
    
    * fixing outdated tests. need to add more

--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -7,8 +7,13 @@ import 'package:swiddledick_news/services/api.dart';
 import 'package:swiddledick_news/views/detail_page.dart';
 import 'package:swiddledick_news/widgets/shimmer.dart';
 
-class MainView extends StatelessWidget {
-  const MainView({Key? key}) : super(key: key);
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MainView();
+}
+
+class MainView extends State<App> {
+  List<Item> items = [];
 
   @override
   Widget build(BuildContext context) {
@@ -20,34 +25,42 @@ class MainView extends StatelessWidget {
         future: _getTopPosts(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            List<Item> items = snapshot.data as List<Item>;
+            items = snapshot.data as List<Item>;
             return Padding(
               padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                itemCount: items.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: ListTile(
-                      title: Text(items[index].title),
-                      subtitle: Text('${items[index].by}  ${items[index].getHoursSincePosted()} hrs'),
-                      trailing: Column(children: <Widget>[
-                        Icon(Icons.comment),
-                        Text('${items[index].descendants}'),
-                      ]),
-                      onTap: () {
-                        print(items[index].id);
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => DetailPage(
-                                    selectedItem: items[index],
-                                  )),
-                        );
-                      },
-                    ),
-                  );
+              child: RefreshIndicator(
+                onRefresh: () async { 
+                  List<Item> newItems = await _getTopPosts();
+                  setState(() {
+                    items = newItems;
+                  });
                 },
-              ),
+                child: ListView.builder(
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: ListTile(
+                        title: Text(items[index].title),
+                        subtitle: Text('${items[index].by}  ${items[index].getHoursSincePosted()} hrs'),
+                        trailing: Column(children: <Widget>[
+                          Icon(Icons.comment),
+                          Text('${items[index].descendants}'),
+                        ]),
+                        onTap: () {
+                          print(items[index].id);
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => DetailPage(
+                                      selectedItem: items[index],
+                                    )),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+              )
             );
           } else {
             return Padding(
@@ -67,8 +80,13 @@ class MainView extends StatelessWidget {
 
   Future<List<Item>> _getTopPosts() async {
     var response = await Api.getTopStories();
-    var storiesDecoded = await json.decode(response.body);
-    var storiesListed = List<int>.from(storiesDecoded.take(30));
-    return Api.getItemList(storiesListed);
+    
+    if (response.statusCode == 200) {
+      var storiesDecoded = await json.decode(response.body);
+      var storiesListed = List<int>.from(storiesDecoded.take(30));
+      return Api.getItemList(storiesListed);
+    }
+
+    return [];
   }
 }
